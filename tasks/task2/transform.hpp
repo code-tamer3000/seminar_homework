355d1bb1774fa9ce887af5e268064efff69f89f9
code-tamer3000 <parfenov-art@mail.ru>
2023-02-15 21:31:06 +0500

    function TransformIf was implemented

--- tasks/task2/transform.hpp
+++ tasks/task2/transform.hpp
@@ -1,4 +1,40 @@
 template <class T>
 void TransformIf(T* begin, T* end, bool (*p)(const T&), void (*f)(T&)) {
-    // Your code goes here
+    T* curr = begin;
+    std::vector<T> hold;
+    while (curr++ != end) {
+        bool succses_p = false;
+        bool succses_copy = true;
+
+        try {
+            hold.push_back(*curr);
+        } catch (...) {
+            succses_copy = false;
+        }
+
+        try {
+            succses_p = p(*curr);
+        } catch(...) {
+            if (!succses_copy) throw;
+            for (int i = 0; i < hold.size(); i++) {
+                T* undo = begin + i;
+                *undo = hold[i];
+            }
+            throw;
+        }
+
+        if (succses_p) {
+            try {
+                f(*curr);
+            } catch (...) {
+                if (!succses_copy) throw;
+                for (int i = 0; i < hold.size(); i++) {
+                    T* undo = begin + i;
+                    *undo = hold[i];
+                }
+                throw;
+            }
+        }
+
+    }
 }
